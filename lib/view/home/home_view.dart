6ef3e20a783bd82591c2d781c14142e73b97ccd9
gabry.zolla@gmail.com <Gabry10>
2022-04-06 09:58:31 +0200

    Views: start working on Home view

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -2,8 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:zweeck/view/home/home_viewmodel.dart';
 
-class HomeView extends StatelessWidget {
-  const HomeView({Key? key}) : super(key: key);
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomeView> createState() => HomeViewState();
+}
+
+class HomeViewState extends State<HomeView> {
+
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      length: 2,
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +37,32 @@ class HomeView extends StatelessWidget {
       onModelReady: (HomeViewModel model) => model.init(),
       builder: (BuildContext context, HomeViewModel model, Widget? child) {
         return Scaffold(
-          body: Center(child: Text(model.posts[0].description),),
+          body: Center(
+            child: Column(
+              children: <Widget>[
+                TabBar(
+                  controller: /*_tabController*/,
+                  tabs: const [
+                    Tab(
+                      text: 'Followed',
+                    ),
+                    Tab(
+                      text: 'For You',
+                    )
+                  ],
+                ),
+                Expanded(
+                  child: TabBarView(
+                    controller: _tabController,
+                    children: const [
+                      ChronometerHomePage(title: 'Chronometer'),
+                      TimerHomePage(title: 'Timer'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         );
       },
     );
